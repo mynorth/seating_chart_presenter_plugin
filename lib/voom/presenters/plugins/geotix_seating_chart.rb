fb8910d6f9057332a927e10d18200194d5f61c91
Tyler Lemburg <tyler@geotix.com>
2019-02-27 11:35:52 -0500

    Change some names

--- lib/voom/presenters/plugins/geotix_seating_chart.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart.rb
@@ -1,23 +1,22 @@
 require_relative 'seating_chart/component'
-require_relative 'seating_chart/action'
 
 module Voom
   module Presenters
     module Plugins
-      module SeatingChart
+      module GeotixSeatingChart
         module DSLComponents
-          def seating_chart(chart_id, **attributes, &block)
-            self << SeatingChart::Component.new(chart_id, parent: self, **attributes, &block)
+          def geotix_seating_chart(chart_id, **attributes, &block)
+            self << GeotixSeatingChart::Component.new(chart_id, parent: self, **attributes, &block)
           end
         end
 
         module WebClientComponents
-          def render_seating_chart(comp,
+          def render_geotix_seating_chart(comp,
                                    render:,
                                    components:,
                                    index:)
             view_dir = File.join(__dir__, 'seating_chart')
-            render.call :erb, :seating_chart, views: view_dir,
+            render.call :erb, :geotix_seating_chart, views: view_dir,
                         locals: {comp: comp,
                                  components: components,
                                  index: index}
--- lib/voom/presenters/plugins/geotix_seating_chart/component.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart/component.rb
@@ -3,12 +3,12 @@ require 'voom/presenters/dsl/components/event_base'
 module Voom
   module Presenters
     module Plugins
-      module SeatingChart
+      module GeotixSeatingChart
         class Component < DSL::Components::EventBase
           attr_reader :chart_id
           def initialize(chart_id, **attribs_, &block)
             @chart_id = chart_id
-            super(type: :seating_chart, **attribs_, &block)
+            super(type: :geotix_seating_chart, **attribs_, &block)
             expand!
           end
         end
