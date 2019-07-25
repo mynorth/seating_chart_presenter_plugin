74d03a0e68f0ed752bfcb463666a867ada626776
Derek Graham <derek@mynorth.com>
2019-07-25 16:51:56 -0400

    Set the language

--- lib/voom/presenters/plugins/geotix_seating_chart/components/designer.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart/components/designer.rb
@@ -6,13 +6,14 @@ module Voom
       module GeotixSeatingChart
         module Components
           class Designer < DSL::Components::EventBase
-            attr_reader :subaccount_id, :designer_key, :chart_key, :disabled, :readonly
+            attr_reader :subaccount_id, :designer_key, :chart_key, :disabled, :readonly, :language
             def initialize(subaccount_id, designer_key, **attribs, &block)
               @subaccount_id = subaccount_id
               @designer_key = designer_key
               @chart_key = attribs.delete(:chart_key){ nil }
               @disabled = attribs.delete(:disabled){ [] }
               @readonly = attribs.delete(:readonly){ [] }
+              @language = attribs.delete(:language){ 'en' }
               super(type: :geotix_seating_designer, **attribs, &block)
               expand!
             end
