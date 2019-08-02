734edf626e0139a06da6c195048aa31549a14dfd
Derek Graham <derek@mynorth.com>
2019-08-02 15:08:13 -0400

    Update chart to take price and formatting parameters.

--- lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
@@ -6,12 +6,16 @@ module Voom
       module GeotixSeatingChart
         module Components
           class Chart < DSL::Components::EventBase
-            attr_reader :subaccount_id, :chart_key, :event_id, :public_key
+            attr_reader :subaccount_id, :chart_key, :event_id, :public_key,
+                        :pricing, :price_format, :price_level_tooltip
             def initialize(subaccount_id, chart_key, public_key, **attribs, &block)
               @subaccount_id = subaccount_id
               @chart_key = chart_key
               @public_key = public_key
               @event_id = attribs.delete(:event_id){ nil }
+              @pricing = attribs.delete(:pricing){ [] }
+              @price_format = attribs.delete(:price_format){ '$' }
+              @price_level_tooltip = attribs.delete(:price_level_tooltip){ 'Select a price level' }
               super(type: :geotix_seating_chart, **attribs, &block)
               expand!
             end
