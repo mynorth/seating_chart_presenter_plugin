f70d2940ed09b8c7c1f56072a4495181828e6c2b
Derek Graham <derek@mynorth.com>
2019-08-07 16:46:52 -0400

    Add available categories parameter for chart component

--- lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
@@ -6,19 +6,20 @@ module Voom
       module GeotixSeatingChart
         module Components
           class Chart < DSL::Components::EventBase
-            attr_reader :subaccount_id, :chart_key, :event_id, :public_key,
-                        :pricing, :price_format, :price_level_tooltip
-            def initialize(subaccount_id, chart_key, public_key, **attribs, &block)
-              @subaccount_id = subaccount_id
+            attr_reader :chart_key, :event_id, :public_key, :pricing, :price_format,
+                        :price_level_tooltip, :available_categories
+            def initialize(chart_key, public_key, **attribs, &block)
               @chart_key = chart_key
               @public_key = public_key
               @event_id = attribs.delete(:event_id){ nil }
               @pricing = attribs.delete(:pricing){ [] }
               @price_format = attribs.delete(:price_format){ '$' }
               @price_level_tooltip = attribs.delete(:price_level_tooltip){ 'Select a price level' }
+              @available_categories = attribs.delete(:available_categories){ [] }
               super(type: :geotix_seating_chart, **attribs, &block)
               expand!
             end
+
           end
         end
       end
