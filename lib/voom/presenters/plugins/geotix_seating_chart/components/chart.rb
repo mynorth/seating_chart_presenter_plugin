54c20c3a54c1bda564c9ac58d845a88ca255c313
Derek Graham <derek@mynorth.com>
2020-03-02 14:31:24 -0500

    Currency and locale -> price formatting as seating chart component params.

--- lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
+++ lib/voom/presenters/plugins/geotix_seating_chart/components/chart.rb
@@ -4,19 +4,20 @@ module Voom
       module GeotixSeatingChart
         module Components
           class Chart < Base
-            attr_reader :chart_key, :event_id, :public_key, :pricing, :price_format,
+            attr_reader :chart_key, :event_id, :public_key, :pricing, :currency, :locale,
                         :price_level_tooltip, :available_categories, :chart_js_url
             def initialize(chart_key, public_key, **attribs, &block)
               @chart_key = chart_key
               @public_key = public_key
               @event_id = attribs.delete(:event_id){ nil }
               @pricing = attribs.delete(:pricing){ [] }
-              @price_format = attribs.delete(:price_format){ '$' }
+              @currency = attribs.delete(:currency){ 'USD' }  # https://www.iban.com/currency-codes
+              @locale = attribs.delete(:locale){ 'en-US' }
               @price_level_tooltip = attribs.delete(:price_level_tooltip){ 'Select a price level' }
               @available_categories = attribs.delete(:available_categories){ [] }
               @chart_js_url = attribs.fetch(:chart_js_url, Settings.config.chart_js_url)
-              @component_options = %i(chart_key event_id public_key pricing price_format
-                                  price_level_tooltip available_categories)
+              @component_options = %i(chart_key event_id public_key pricing currency
+                                      locale price_level_tooltip available_categories)
               super(type: :geotix_seating_chart, **attribs, &block)
               expand!
             end
